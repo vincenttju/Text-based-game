d55a1f531b1b51e711727c4073dbe5c179212682
Hafi08 <61740310+Hafi08@users.noreply.github.com>
2020-05-01 00:28:48 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,11 +8,12 @@ void mainMenu(){
   getline(cin, name);
   cout << "You woke up with a sick head lying on the floor" << endl;
   cout << "in a dark room filled with an unpleasant odor " << endl;
-  cout << "\nYou have no idea how you got here" << endl;
+  cout << "\nYou have no idea of who you are and how you got here" << endl;
   cout << "You see a scroll on your left saying:" << endl;
   cout << "---------------------------------------------------" << endl;
   cout << "Find 4 adventure items scattered around the mansion" << endl;
   cout << "     and use it in the ancient adventure gate" << endl;
+  cout << "  Be careful of him, human flesh is his favourite  " << endl;
   cout << "---------------------------------------------------" << endl;
 }
 int main(){
