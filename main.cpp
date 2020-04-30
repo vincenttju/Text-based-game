e0640f8984579f249368005fa4dd7945cfc0b660
Vincent <h3566353@academy11.cs.hku.hk>
2020-05-01 00:14:31 +0800

    Added intro storyline

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 using namespace std;
 
+void mainMenu(){
+  string name;
+  cout << "************ Welcome to One Night Game ************" << endl;
+  cout << "Hello, please enter your character name." << endl;
+  getline(cin, name);
+  cout << "You woke up with a sick head lying on the floor" << endl;
+  cout << "in a dark room filled with an unpleasant odor " << endl;
+  cout << "\nYou have no idea how you got here" << endl;
+  cout << "You see a scroll on your left saying:" << endl;
+  cout << "---------------------------------------------------" << endl;
+  cout << "Find 4 adventure items scattered around the mansion" << endl;
+  cout << "     and use it in the ancient adventure gate" << endl;
+  cout << "---------------------------------------------------" << endl;
+}
 int main(){
+  int map[3][3];
+  mainMenu();
 }
