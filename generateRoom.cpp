e620b9de0c913edae370e619a4db93a2dc5a8883
Vincent <h3566353@academy11.cs.hku.hk>
2020-05-02 01:13:20 +0800

    minor fix

--- generateRoom.cpp
+++ generateRoom.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "generateRoom.H"
+#include "generateRoom.h"
 using namespace std;
 
 void generateRoom(int Y, int X){
   int roomNumber = (X + 1) + (Y*3);
-  string rN = to_string(roomNumber); 
+  string rN = to_string(roomNumber);
   string roomfile = "room" + rN  + ".txt";
-  
+
   ifstream fin;
   string message;
   fin.open(roomfile);
   while (getline(fin, message)){
     cout << message << endl;
   }
-        
+
   fin.close();
 }
-    
