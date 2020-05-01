870245ef3b03ceb8fe694f0df9ae8cd496cc9aed
Hafi08 <61740310+Hafi08@users.noreply.github.com>
2020-05-02 02:25:22 +0700

    Update go_command.cpp

--- go_command.cpp
+++ go_command.cpp
@@ -3,9 +3,11 @@
 #include "generateRoom.h"
 using namespace std;
 
-void go_command(int &y, int &x, string userInput, int map[][3]){
+void go_command(int &y, int &x, string userInput){
+  int roomNumber = ((x+1)+(y*3));
+  
   if (userInput == "go north"){
-    if (map[y][x] == 1 || map[y][x] == 2 || map[y][x] == 3){
+    if (roomNumber == 1 || roomNumber == 2 || roomNumber == 3){
       cout << "----- You cannot go there -----" << endl;
       doCommand();
     }
@@ -14,17 +16,38 @@ void go_command(int &y, int &x, string userInput, int map[][3]){
       x += 0;
     }
   }
+  
   else if (userInput == "go south"){
-    y -= 1;
-    x += 0;
+    if (roomNumber == 7 || roomNumber == 8 || roomNumber == 9){
+      cout << "----- You cannot go there -----" << endl;
+      doCommand();
+    }
+    else{
+      y += 1;
+      x += 0;
+    }
   }
+  
   else if (userInput == "go east"){
-    y += 0;
-    x += 1;
+    if (roomNumber == 3 || roomNumber == 6 || roomNumber == 9){
+      cout << "----- You cannot go there -----" << endl;
+      doCommand();
+    }
+    else{
+      y += 0;
+      x += 1;
+    }
   }
+  
   else if (userInput == "go west"){
-    y += 0;
-    x += 1;
+    if (roomNumber == 1 || roomNumber == 4 || roomNumber == 7){
+      cout << "----- You cannot go there -----" << endl;
+      doCommand();
+    }
+    else{
+      y += 0;
+      x -= 1;
+    }
   }
   generateRoom(y, x);
 }
