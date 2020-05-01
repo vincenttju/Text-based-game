fb6ac5ca43f97aeebadd6d1023f84f490d33dd3f
Hafi08 <61740310+Hafi08@users.noreply.github.com>
2020-05-02 01:20:15 +0700

    Create goCommand.cpp

---
+++ goCommand.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void goCommand(int &y, int &x, string userInput){
+  int roomNumber = (x + 1) + (y*3);
+  if (userInput == "go north"){
+    y -= 1;
+    x += 0;
+  }
+  else if (userInput == "go south"){
+    y -= 1;
+    x += 0;
+  }
+  else if (userInput == "go east"){
+    y += 0;
+    x += 1;
+  }
+  else if (userInput == "go west"){
+    y += 0;
+    x += 1;
+  }
+  generateRoom(y, x);
+}
