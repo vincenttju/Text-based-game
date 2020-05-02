efeef23b85153333206b560878c1d47f6a2e6ffc
vincenttju <61791201+vincenttju@users.noreply.github.com>
2020-05-02 14:29:39 +0700

    Create commands.cpp

---
+++ commands.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void take_command(){
+
+}
+
+void examine_command(){
+
+}
+
+void open_command(){
+
+}
+
+void use_command(){
+
+}
+
+void answer_command(){
+
+}
