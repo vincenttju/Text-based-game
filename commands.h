974859486e2beb5eb7f2a645af8a6d962f0d9796
vincenttju <61791201+vincenttju@users.noreply.github.com>
2020-05-06 21:40:32 +0700

    Create commands.h

---
+++ commands.h
@@ -0,0 +1,10 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+#include <string>
+using namespace std;
+
+void go_command(int &y, int &x, string userInput);
+
+void take_command(int y, int x, string invertory[], string itemTaken, int &count, struct listItems love[]);
+
+#endif
