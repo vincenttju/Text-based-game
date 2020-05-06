9d3401b1690a791482b8eb57b293cef779ed57e2
vincenttju <61791201+vincenttju@users.noreply.github.com>
2020-05-06 21:21:55 +0700

    Removed command checker, and move to other files

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <cctype>
 #include "generateRoom.h"
 #include "go_command.h"
+#include "commandCheck.h"
 using namespace std;
 
 struct listItems{
@@ -41,59 +42,6 @@ void mainMenu(){
     cout << "------> Please input your command : " << endl;
     getline(cin, firstInput);
   }
-}
-
-bool nounCheck(string commands[]){
-  string nounList[18] = {"north", "south", "east", "west","key", "rope", "torch", "phone", "gun", "flashlight", "picture", "letter", "pan", "fruits", "bullet", "battery", "mail", "game"};
-  int nounFound = 0;
-  for (int i = 0; i < 18; i++){
-    if (commands[1] == nounList[i])
-    {
-      nounFound++;
-      break;
-    }
-  }
-
-  if (nounFound == 0){
-    cout << "\n--------------- Invalid Noun --------------" << endl;
-    cout <<   "------------- Some Valid Noun: -----------" << endl;
-    cout <<   "--- game, key, rope, torch, phone, gun  --" << endl;
-    return true;
-  }
-  return 0;
-}
-
-bool commandCheck(string commands[]){
-  for (int i = 0 ; i < 2; i++){
-    for (int j = 0; j < commands[i].length();j++){
-      if (isupper(commands[i][j])){
-        cout << "\n------------- Invalid Command -------------" << endl;
-        cout <<   "------Command should be in lowercase-------" << endl;
-        return true;
-      }
-    }
-  }
-  return 0;
-}
-
-bool commandCheck2(string commands[]){
-  string commandsList[7] = {"go", "take", "examine", "open", "use", "answer", "quit"};
-  int commandFound = 0;
-  for ( int i = 0; i < 7; i++){
-    if (commands[0] == commandsList[i])
-    {
-      commandFound++;
-      break;
-    }
-  }
-
-  if (commandFound == 0){
-    cout << "\n------------- Invalid Command -------------" << endl;
-    cout <<   "-------------  Valid commands: ------------" << endl;
-    cout <<   " go, take, examine, open, use, answer, quit" << endl;
-    return true;
-  }
-  return 0;
 }
 
 void command(int &count, string commands[]){
