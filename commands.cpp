5dcd9ce0b28888197615e05589d1033ecdf376ae
vincenttju <61791201+vincenttju@users.noreply.github.com>
2020-05-06 21:40:36 +0700

    Update commands.cpp

--- commands.cpp
+++ commands.cpp
@@ -1,8 +1,72 @@
 #include <iostream>
+#include <string>
+#include "generateRoom.h"
+#include "commands.h"
 using namespace std;
 
-void take_command(){
+void go_command(int &y, int &x, string userInput){
+  int roomNumber = ((x+1)+(y*3));
+  
+  if (userInput == "north"){
+    if (roomNumber == 1 || roomNumber == 2 || roomNumber == 3){
+      cout << "----- You cannot go there -----" << endl;
+      
+    }
+    else{
+      y -= 1;
+      x += 0;
+      generateRoom(y, x);
+    }
+  }
+  
+  else if (userInput == "south"){
+    if (roomNumber == 7 || roomNumber == 8 || roomNumber == 9){
+      cout << "----- You cannot go there -----" << endl;
+      
+    }
+    else{
+      y += 1;
+      x += 0;
+      generateRoom(y, x);
+    }
+  }
+  
+  else if (userInput == "east"){
+    if (roomNumber == 3 || roomNumber == 6 || roomNumber == 9){
+      cout << "----- You cannot go there -----" << endl;
+      
+    }
+    else{
+      y += 0;
+      x += 1;
+      generateRoom(y, x);
+    }
+  }
+  
+  else if (userInput == "west"){
+    if (roomNumber == 1 || roomNumber == 4 || roomNumber == 7){
+      cout << "----- You cannot go there -----" << endl;
+      
+    }
+    else{
+      y += 0;
+      x -= 1;
+      generateRoom(y, x);
+    }
+  }
+  
+}
 
+void take_command(int y, int x, string invertory[], string itemTaken, int &count, struct listItems love[]){
+  int roomNumber = ((x)+(y*3));
+  
+  if ((itemTaken == love[roomNumber].item1) || (itemTaken == love[roomNumber].item2)){
+    invertory[count] += itemTaken;
+    count += 1;
+  }
+  else{
+    cout << "invalid items!" << endl;
+  }
 }
 
 void examine_command(){
