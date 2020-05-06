fea9129825ab3def916287aa4a00891559be48b2
Hafi08 <61740310+Hafi08@users.noreply.github.com>
2020-05-06 21:12:10 +0700

    Update take_command.cpp

--- take_command.cpp
+++ take_command.cpp
@@ -2,10 +2,10 @@
 #include <string>
 using namespace std;
 
-void take_command(int y, int x, string invertory[], string itemTaken, int &count, struct listItems test[]){
+void take_command(int y, int x, string invertory[], string itemTaken, int &count, struct listItems love[]){
   int roomNumber = ((x)+(y*3));
   
-  if ((itemTaken == test[roomNumber].item1) || (itemTaken == test[roomNumber].item2)){
+  if ((itemTaken == love[roomNumber].item1) || (itemTaken == love[roomNumber].item2)){
     invertory[count] += itemTaken;
     count += 1;
   }
