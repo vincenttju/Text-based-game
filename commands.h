1d20ec8663cf80054d0c6d0081af8338618d5866
Vincent <61791201+vincenttju@users.noreply.github.com>
2020-05-07 19:45:38 +0700

    Update commands.h

--- commands.h
+++ commands.h
@@ -6,8 +6,8 @@ using namespace std;
 
 void go_command(int &y, int &x, string userInput);
 
-void take_command(int y, int x, string * &invertory, string itemTaken, int &itemsCarried, int &inventorySize, listItems love[]);
+void take_command(int y, int x, string * &inventory, string itemTaken, int &itemsCarried, int &inventorySize, listItems love[]);
 
-void view_invertory_command(string * &invertory, int inventorySize, string itemViewed);
+void view_inventory_command(string * &inventory, int inventorySize, string itemViewed);
 
 #endif
