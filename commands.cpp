749096e9c0b31da615e61ce927991098116fdb23
Vincent <h3566353@academy11.cs.hku.hk>
2020-05-07 22:59:58 +0800

    Fixed some files

--- commands.cpp
+++ commands.cpp
@@ -123,15 +123,16 @@ void item_function(string item){
     cout << " with a big and gloomy feature " << endl;
     cout << " There is a huge \"A\" character in the background of the picture " << endl;
   }
-  
+
   else if (item == "fruits"){
     cout << " You have a bunch of rotten fruits " << endl;
     cout << " banana, apple, mango, and orange  " << endl;
     cout << " You cannot eat these garbage! " << endl;
   }
-  
-  else{
+
+  else
     cout << "This item cannot be examined" << endl;
+}
 
 void examine_command(int y, int x, string itemExamined, string * &inventory, int inventorySize){
   int count1=0;
