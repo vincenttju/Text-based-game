e5d9710585f03c1754b4e2cabe9607f968fda0b8
Hafi08 <61740310+Hafi08@users.noreply.github.com>
2020-05-07 23:24:01 +0700

    Update mainMenu.cpp

--- mainMenu.cpp
+++ mainMenu.cpp
@@ -3,24 +3,21 @@
 using namespace std;
 
 void mainMenu(){
-  string name, firstInput;
-  cout << "************ Welcome to One Night Game ************" << endl;
-  cout << "Hello, please enter your name." << endl;
-  getline(cin, name);
-  cout << "Hello " << name << ", good luck and have fun" << endl;
+  string firstInput;
+
   cout << "\nOn one unpleasant day, you woke up with a sick head lying on the floor" << endl;
   cout << "in a dark room filled with an unpleasant odor " << endl;
-  cout << "\nYou have no idea of who you are and how you got here" << endl;
+  cout << "You have no idea of who you are and how you got here" << endl;
   cout << "You see a strange scroll on your left saying:" << endl;
-  cout << "---------------------------------------------------" << endl;
-  cout << "If you read this message, I probably have already dead" << endl;
-  cout << "      You need to get out of this creepy mansion      " << endl;
-  cout << "           or you will be dead by one night           " << endl;
-  cout << "  Find the unique items scattered around the mansion  " << endl;
-  cout << "            and use it in the ancient gate            " << endl;
-  cout << "   Find several pieces of clues to get out of here    " << endl;
-  cout << "   Be careful of him, human flesh is his favourite    " << endl;
-  cout << "------------------------------------------------------" << endl;
+  cout << "---------------------------------------------------------" << endl;
+  cout << "If you read this message, I probably have already dead   " << endl;
+  cout << "      You need to get out of this creepy mansion         " << endl;
+  cout << "           or you will be dead by one night              " << endl;
+  cout << "  Find the unique items scattered around the mansion     " << endl;
+  cout << " keep it in your inventory and use it in the ancient gate" << endl;
+  cout << "   Find several pieces of clues to get out of here       " << endl;
+  cout << "   Be careful of him, human flesh is his favourite       " << endl;
+  cout << "---------------------------------------------------------" << endl;
   cout << "There is a door in your north side" << endl;
   cout << "You can go north" << endl;
   cout <<  "------> Please input your command : " << endl;
