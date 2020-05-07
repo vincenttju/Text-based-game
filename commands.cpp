11e98a7ee5aece3c9cb3122cfd7a8e0c11a5c1da
Hafi08 <61740310+Hafi08@users.noreply.github.com>
2020-05-07 21:02:12 +0700

    Update commands.cpp

--- commands.cpp
+++ commands.cpp
@@ -74,47 +74,6 @@ void take_command(int y, int x, string * &inventory, string itemTaken, int &item
     cout << "invalid items!" << endl;
   }
 }
-
-void item_function(string item){
-	if (item == "letter"){cout << "________________________________________________" << endl;
-    cout << endl;
-    cout << " If you find this message, keep it in mind      " << endl;
-    cout << " IT IS ALWAYS WATCHING YOU! The bloody creature " << endl;
-    cout << " will kill you in the blink of an eye. " << endl;
-    cout << " If it shows up, use a flash light or torch     " << endl;
-    cout << " Enlightment is its weakness  " << endl;
-    cout << " Find also gun and the bullet, it wont kill him " << endl;
-    cout << " but it wil slow him down " << endl;
-    cout << "________________________________________________" << endl;
-	}
-	
-	else if (item == "notepad"){
-		cout << "___________________________________________________________" << endl;       
-    cout << endl;
-		cout << " 1. I TURN ONCE, WHAT IS OUT WILL NOT GET IN, " << endl;
-		cout << "    I TURN AGAIN, WHAT IS IN WILL NOT GET OUT. " << endl;
-		cout << endl;
-		cout << " 2. I CAN FILL AN ENTIRE ROOM WITHOUT TAKING UP ANY SPACE. " << endl;
-		cout << endl;
-		cout << " 3. THE MORE OF THIS THERE IS, THE LESS YOU SEE." << endl;
-		cout << "___________________________________________________________" << endl;
-	}
-	
-	else if (item == "mail"){
-		cout << "_______________________________________________________" << endl;
-		cout << endl;
-		cout << " You can't open the gate without me! " << endl;
-		cout << " I follow you all the time and I know your every move, " << endl;
-		cout << " but you can’t never touch me or catch me." << endl;
-		cout << "_______________________________________________________" << endl;
-	}
-
-	else if (item == "picture"){
-		cout << " It is a family black and white photograph " << endl;
-		cout << " In the photo you can see a mysterious guy " << endl;
-		cout << " with a big and gloomy feature " << endl;
-	}
-}
   
 
 void examine_command(int y, int x, string itemExamined, string * &inventory, int inventorySize){
