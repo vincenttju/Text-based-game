689e80772d96dcf245709b6016bd8856434f0105
Hafi08 <61740310+Hafi08@users.noreply.github.com>
2020-05-09 00:36:13 +0700

    adding comment on what the function does

--- generateRoom.cpp
+++ generateRoom.cpp
@@ -4,6 +4,10 @@
 #include "generateRoom.h"
 using namespace std;
 
+// This function will generate room from each 9 room by using File I/O
+// By using input int y and x as the current location of the user, 
+// it will read from another txt file and then output the room text
+
 void generateRoom(int y, int x){
   
   int roomNumber = ((x+1)+(y*3));
