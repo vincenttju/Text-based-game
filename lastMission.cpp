e0221349bd85eae9211dda5f09af0ffb02abe81e
Vincent <61791201+vincenttju@users.noreply.github.com>
2020-05-08 21:46:01 +0700

    Update lastMission.cpp

--- lastMission.cpp
+++ lastMission.cpp
@@ -20,20 +20,21 @@ bool last_Mission(){
       cin >> answer;
       counter++;
     }
-    cout << "\n===                        The password door is unlocked!                     ===" << endl;
-    cout << "===                           You have tried " << counter + 1 << " times!                         ===" << endl;
-    cout << "===  As soon as you opened the door, a hell creature is coming in your way!   ===" << endl;
-    cout << "===             What would you choose to kill him? (gun / knife)              ===" << endl;
+    cout << "\n===                       The password door is unlocked!                     ===" << endl;
+    cout << "===                          You have tried " << counter + 1 << " times!                         ===" << endl;
+    cout << "===  As soon as you opened the door, a hell creature is coming in your way!  ===" << endl;
+    cout << "===             What would you choose to kill him? (gun / knife)             ===" << endl;
     cout << "Your choice (gun / knife) : " << endl;
 
     cin >> choice;
     if (choice == "gun"){
-      cout << "\n===           You shot him right on his heart with the silicone bullet!            ===" << endl;
-      cout << "===        He is dead now, but not forever, you still have to find the way out!    ===" << endl;
+      cout << "===                  You loaded your gun with some bullets                   ===" << endl;
+      cout << "\n===        You shot him right on his heart with the silicone bullet!         ===" << endl;
+      cout << "===    He is dead now, but not forever, you still have to find the way out!  ===" << endl;
     }
     else if (choice == "knife"){
-      cout << "\n===               The creature is too fast to be striked by a knife.          ===" << endl;
-      cout << "===                              You are killed instead!                     ===" << endl;
+      cout << "\n===               The creature is too fast to be striked by a knife.         ===" << endl;
+      cout << "===                          You are killed instead!                         ===" << endl;
       cout << "====================               GAME OVER!               ====================" << endl;
       return 0;
     }
@@ -41,11 +42,11 @@ bool last_Mission(){
       cout << "\n===                    You did not choose the right weapon                   ===" << endl;
       cout << "===         The creature is too fast, you have no other opportunity          ===" << endl;
       cout << "===                            You are killed instead!                       ===" << endl;
-      cout << "====================             GAME OVER!                =====================" << endl;
+      cout << "====================               GAME OVER!              =====================" << endl;
       return 0;
     }
 
-    cout << "\n===                You have escaped from the creepy mansion!                 ===" << endl;
+    cout << "\n===               You have escaped from the creepy mansion!                  ===" << endl;
     cout << "===               But you realised that you are on the second floor!         ==="<< endl;
     cout << "===                   Maybe try to use the rope that you got?                ===" << endl;
     getline(cin, useRope);
@@ -55,11 +56,13 @@ bool last_Mission(){
         getline(cin, useRope);
       }
     }
-    cout << "\n===              You have finally escaped the creepy mansion!                    ===" << endl;
-    cout << "===                    You use your phone to call for help                       ===" << endl;
-    cout << "=== Help service arrived 3 hours later, and you are back to your normal life!    ===" << endl;
-    cout << "====================             END GAME!               ===========================" << endl;
-    cout << "===============        Thank you for playing the game!        ======================" << endl;
+    cout << "\n===              You have finally escaped the creepy mansion!                ===" << endl;
+    cout << "===    Luckily you have collected the battery, lets put it into the phone.   ===" << endl;
+    cout << "===                             The phone is on!                             ===" << endl;
+    cout << "===                   You use your phone to call for help                    ===" << endl;
+    cout << "=== Help service arrived 3 hours later, and you are back to your normal life!===" << endl;
+    cout << "====================             END GAME!             =========================" << endl;
+    cout << "===============      Thank you for playing the game!      ======================" << endl;
     string rating;
     cout << " - Please rate the game : (0-10) -" << endl;
     getline(cin, rating);
