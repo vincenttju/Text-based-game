a0de5fe12c0b2c01c152a6261904f01bb06a5c01
Hafi08 <61740310+Hafi08@users.noreply.github.com>
2020-05-08 16:04:06 +0700

    Update generateRoom.cpp

--- generateRoom.cpp
+++ generateRoom.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "generateRoom.h"
+using namespace std;
+
+void generateRoom(int y, int x){
+  
+  int roomNumber = ((x+1)+(y*3));
+  string rN = to_string(roomNumber);
+  string roomfile = "room" + rN  + ".txt";
+  cout << endl;
+  
+  ifstream fin;
+  string message;
+  fin.open(roomfile);
+  while (getline(fin, message)){    
+    cout << message << endl;
+  }
+
+  fin.close();
+}
 
