fc9d7f602313b7a02f427d54b218177bfbd699a8
Vincent <61791201+vincenttju@users.noreply.github.com>
2020-05-09 11:22:50 +0700

    Added comments to function

--- commandCheck.cpp
+++ commandCheck.cpp
@@ -3,6 +3,7 @@
 #include "commandCheck.h"
 using namespace std;
 
+// this function check if the noun (second word of the input) is in the noun list (there are 23 correct nouns that can be used in the game)
 bool nounCheck(string commands[]){
   string nounList[23] = {"north", "south", "east", "west","drawer","key", "rope", "torch", "phone", "gun", "flashlight", "picture", "letter", "pan", "fruits", "bullet", "battery", "mail", "game", "notepad", "knife", "lock", "inventory"};
   int nounFound = 0;
@@ -23,6 +24,7 @@ bool nounCheck(string commands[]){
   return 0;
 }
 
+// this function check if the input is all in lowercase
 bool commandCheck(string commands[]){
   for (int i = 0 ; i < 2; i++){
     for (int j = 0; j < commands[i].length();j++){
@@ -36,6 +38,7 @@ bool commandCheck(string commands[]){
   return 0;
 }
 
+// this function check if the command (first word of the input) is in the command list (there are 7 correct commands)
 bool commandCheck2(string commands[]){
   string commandsList[7] = {"go", "take", "examine","view", "use", "answer", "quit"};
   int commandFound = 0;
