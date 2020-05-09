6696509f229664037c7fd3b52d6ab1c58d7e304b
Vincent <61791201+vincenttju@users.noreply.github.com>
2020-05-09 11:37:12 +0700

    Added comments for each functions

--- main.cpp
+++ main.cpp
@@ -14,8 +14,12 @@ using namespace std;
 
 
 void command(int &count, string commands[]){
+  // this function will check whether the command input (stored in commands[]) is correct
+  // count is used to check the number of words of the input
   string command, word;
   bool x = true;
+  
+  // asking for user input until the input is correct (2 words long)
   while(x){
     cout <<  "\n------> Please input your command : " << endl;
     getline(cin, command);
@@ -33,10 +37,16 @@ void command(int &count, string commands[]){
       cout << "\n------------- Invalid Command -------------" << endl;
       cout <<   "------Command should be 2 words long-------" << endl;
     }
+    
+    // check if the input is all in lowercase
     if (commandCheck(commands))
       count = 0;
+    
+    // check if the command (first word of the input) is in the command list (there are 7 correct commands)
     if (commandCheck2(commands))
       count = 0;
+    
+    // check if the noun (second word of the input) is in the noun list (there are 23 correct nouns that can be used in the game)
     if (nounCheck(commands))
       count = 0;
   }
@@ -44,13 +54,23 @@ void command(int &count, string commands[]){
 
 bool doCommand(int &y, int &x, string * &inventory, int &itemsCarried, int &inventorySize, struct listItems love[], int &usedItemcount, string usedItems[], bool &lastMission){
   string commands[2];
+  // this function will take user input, check it, and execute it
+  // x and y inputs are used in several functions for locating the map
+  // inventory is a dynamic array of size 3 to store the items taken (take() function), and the item stored can be used (use() function)
+  // itemsCarried will count every items taken, inventorySize is the size of the dynamic array (start with 3)
+  // if itemsCarried size >= inventorySize, it will grow the dynamic array size
+  // the struct array contains information of items in every room
+  // usedItems will count the number of items used in (use() function) and store it in usedItems array
+  // lastMission is a bool (false), it will be true if some requirement in (use() function) is satisfied
+  
   int count = 0;
-
+  // keep asking for input until it satisfied the requirement
   while (count != 2){
     count = 0;
     command(count, commands);
   }
-
+  
+  // quit and finish the game if the input is "quit"
   if (commands[0] == "quit")
     return 0;
 
@@ -76,6 +96,8 @@ bool doCommand(int &y, int &x, string * &inventory, int &itemsCarried, int &inve
 }
 
 void game(int Yposition, int Xposition){
+  // this function store all of the required information for the game
+  
   listItems love[9];
   love[0] = { 1, "key", "flashlight" };
   love[1] = { 2, "picture" , "phone" };
@@ -91,7 +113,8 @@ void game(int Yposition, int Xposition){
   string * inventory = new string [inventorySize];
   string usedItems[5];
   bool lastMission = false;
-
+  
+  // keep using doCommand() function until it encounters the lastMission (return 0) or the user quit game (return 0)
   while (doCommand(Yposition, Xposition, inventory, itemsCarried, inventorySize, love, usedItemCount,usedItems, lastMission));
 
   delete []inventory;
